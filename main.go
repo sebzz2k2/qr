f74e9cb67343384540c7069f8a7cf1e9a4bbf300
sebin <sebinsebzz2002@gmail.com>
2024-08-12 10:30:15 +0530

    add get version

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-func main() {
+func getQrVersion(val int) int {
 	maxChar := [40]int{
 		16, 29, 47, 67, 87, 108, 125, 157, 189, 221,
 		259, 296, 352, 376, 426, 470, 531, 574, 644, 702,
@@ -10,5 +13,18 @@ func main() {
 		1499, 1618, 1700, 1787, 1867, 1966, 2071, 2181, 2298, 2420,
 	}
 
-	fmt.Println(maxChar)
+	for i := 0; i < len(maxChar); i++ {
+		if maxChar[i] >= val {
+			return i + 1
+		}
+	}
+	return 0
+}
+func main() {
+	str := "HELLO WORLD"
+	qrVer := getQrVersion(len(str))
+	if qrVer == 0 {
+		log.Fatalf(`Sting is not encodeable`)
+	}
+	fmt.Println(qrVer)
 }
